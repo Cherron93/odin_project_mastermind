1533f223e0ff2c4f1e3fd22c2e2b317f889b2b3c
Cherron93 <colinherron93@gmail.com>
2022-12-19 13:53:04 -0500

    Fix nil error in replace_duplicates

--- mastermind.rb
+++ mastermind.rb
@@ -9,6 +9,7 @@ class Mastermind
     @clue = []
     @num_of_guesses = 0
     @answer = []
+    @game_over = false
     define_game_rules
     if @mastermind == 'player'
       puts 'Set the code'
@@ -30,52 +31,74 @@ class Mastermind
 
   # Plays game with player as mastermind
   def player_mastermind
-    if @max_tries.positive?
-      if 0 < @num_of_guesses
-        puts 'Give a clue'
-        @clue = gets.chomp.split(' ')
+    while @game_over == false
+      if @max_tries.positive?
+        if 0 < @num_of_guesses
+          puts 'Give a clue'
+          @clue = gets.chomp.split(' ')
+        end
+        computer_takes_guess
+        @num_of_guesses += 1
+        @max_tries -= 1
+        player_mastermind
+      else
+        puts 'Mastermind wins!'
+        @game_over = true
       end
-      computer_takes_guess
-      @num_of_guesses += 1
-      @max_tries -= 1
-      player_mastermind
-    else
-      puts 'Mastermind wins!'
     end
   end
 
   # Computer guesses the code
   def computer_takes_guess
+    # binding.pry
     if @clue == []
       @answer = @@colors.sample(4)
-      p "Computer answer: #{@answer}"
     else
       right_colors = @clue.reduce(0) { |sum, color| sum += 1 if %w[black white].include?(color) }
       @answer.pop(4 - right_colors)
-      (4 - right_colors).times { @answer.push(@@colors.sample(1)) }
-      p "Computer answer: #{@answer.flatten!}"
+      (4 - right_colors).times do
+        @answer.push(@@colors.sample(1))
+      end
+      @answer = @answer.flatten!
+      replace_duplicates
+      @answer.shuffle!
     end
+    p "Computer answer: #{@answer}"
+  end
+
+  # checks to make sure there are no duplicate colors in computer guess
+  def replace_duplicates
+    # binding.pry
+    @answer.uniq!
+    while @answer.length < 4
+      @answer.push(@@colors.sample(1))
+      @answer.flatten!
+      @answer.uniq!
+    end
+    @answer
   end
 
   # Players game with computer as mastermind
   def computer_mastermind
-    if @max_tries.positive?
+    while @game_over == false
+      next unless @max_tries.positive?
+
       puts 'Enter the code'
       @answer = gets.chomp.split(' ')
       check_answer(@answer)
       @max_tries -= 1
-      puts "#{@max_tries} tries remaining."
+      # puts "#{@max_tries} tries remaining."
       computer_mastermind
-    else
-      puts 'Mastermind wins!'
     end
+    puts 'Mastermind wins!'
   end
 
   # Check if answer matches code
   def check_answer(answer)
     if answer == @code
       puts 'Game over! Codebreaker wins!'
-      @max_tries = 0
+      @game_over = true
+      # @max_tries = 0
     else
       give_clue(@answer)
     end
@@ -83,6 +106,7 @@ class Mastermind
 
   # Give clue based on answer
   def give_clue(answer)
+    binding.pry
     @clues = []
     answer.each do |answer_color|
       answer_index = answer.find_index(answer_color)
@@ -92,7 +116,7 @@ class Mastermind
         @clues.push('white')
       end
     end
-    p @clues
+    p @clues.shuffle
   end
 end
 
